19ea8ebd493ecc2521e5073cb7e10e45fefff944
seog-jun <shong69@myseneca.ca>
2023-03-18 11:49:59 -0400

    Added API call using fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+  }, []);
+  console.log(movies);
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
 export default App;
