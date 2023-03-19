51dbe5317f21130f86c405db74b65c1c1f51d974
seog-jun <shong69@myseneca.ca>
2023-03-18 22:59:55 -0400

    Refactored codes using the latest version of react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/movie">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/movie" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </Router>
   );
 }
