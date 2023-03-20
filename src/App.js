1053b8e3219e5ff7bde777a869a3b340fd6ba219
seog-jun <shong69@myseneca.ca>
2023-03-19 23:36:34 -0400

    Modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/movie/:id" element={<Detail />}></Route>
