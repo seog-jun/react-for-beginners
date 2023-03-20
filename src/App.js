3fbc98ebc172e7f721e1aa43a2bb697c219a902d
seog-jun <shong69@myseneca.ca>
2023-03-19 23:34:27 -0400

    Modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 function App() {
   return (
-    <Router basename="react-for-beginners">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/movie/:id" element={<Detail />}></Route>
